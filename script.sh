3d31ed1f671b3a59fb94961652b47bf8609a96b8
KRUGER\ecordova <ecordova@kruger.com.ec>
2019-09-24 16:45:07 -0500

    first commit

---
+++ script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+NOMBRE=$1
+APELLIDO=$2
+MOSTRAR=$3
+if [ "$MOSTRAR" = "true" ]; then
+	echo "Hola, $NOMBRE $APELLIDO"
+else
+	echo "Si quieres ver el nombre, selecciona la cailla de MOSTRAR"
+fi
